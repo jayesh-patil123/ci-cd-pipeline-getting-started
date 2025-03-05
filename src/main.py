7d1d11fdb299a642dcc99be35a157e5f46a8b8f4
jayesh-patil123 <patiljayesh6908@gmail.com>
2025-03-05 16:17:45 +0530

    added new function and testcase

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
 def add(a, b):
     return a+b
+
+def substract(a, b): #new
+    return a-b
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,11 @@
-from src.main import add
+from src.main import add, substract
 
 def test_add_function():
     assert add(2, 3) == 5
-    assert add(0, 0) == 1
+    assert add(0, 0) == 0
     assert add(5, 5) == 10
 
+def test_substract_function():
+    assert substract(5, 3) == 2
+    assert substract(0, 0) == 0
+    assert substract(10, 5) == 5
